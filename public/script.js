adf63677e596c5fd45dd4e50aeef0a0525059973
Thomas Glover <thomasglover@Thomass-MacBook-Pro.local>
2021-05-17 22:01:07 -0400

    switched APIs

--- public/script.js
+++ public/script.js
@@ -1,30 +1,65 @@
-const teamsUrl = "http://api.cfl.ca/v1/teams"
-const standingsUrl = "http://api.cfl.ca/v1/standings/2019"
-const key = '4ONu9LoTyNnwqUOsE9C5QEuIQ036pDZ6'
-const urlWithKey = `${teamsUrl}?key=${key}`
+const baseUrl = "http://api.airvisual.com/v2/city";
+const key = "7403a02f-d63f-46d0-9d39-3c34e76d5bdf";
 
-const teamList = document.querySelector('ul');
+const userCity = document.querySelector('.city');
+const userState = document.querySelector('.state');
+const form = document.querySelector('.form');
+const submitButton = document.getElementsByClassName('.btn');
+const paragraph = document.querySelector('p');
 
-function displayTeams(x){
-    // let teamName = x.data.full_name;
-    let teamLogo = x.data.images.logo_image_url;
-    for(let i = 0; i < x.data.length; i++){
-        let listItem = document.createElement("img");
-        img.className = "list-group-item";
-        listItem.src = teamLogo;
-        teamList.appendChild(listItem);
+
+form.addEventListener('submit', getAQI);
+
+function getAQI(event){
+    event.preventDefault();
+    let url = `${baseUrl}?city=${userCity.value}&state=${userState.value}&country=USA&key=${key}`;
+
+
+    if(userCity.value)
+
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        console.log(json);
+        displayAQI(json);
+    })
+    .catch(function(error){
+        paragraph.innerText = "Um...";
+        smallPara.innerText = "what's that?"
+    })
+
+}
+
+function displayAQI(json){
+    
+    let smallPara = document.getElementById('smallPara');
+    let AQI = json.data.current.pollution.aqius;
+    console.log(AQI);
+    
+    
+
+    if(AQI <= 50){
+        paragraph.innerText = `${AQI}!`;
+        smallPara.innerText = "Nice."
         
     }
-    
+
+        else if(AQI > 50 && AQI < 100){
+            paragraph.innerHTML = `${AQI}!`;
+            smallPara.innerHTML = "Eh. Average."
+    }
+
+        else if(AQI >= 100 && AQI < 200){
+            paragraph.innerHTML = `${AQI}!`;
+            smallPara.innerHTML = "Not good at all."
+
+    }
+        else if(AQI >= 200){
+            paragraph.innerHTML = `${AQI}!`;
+            smallPara.innerHTML = "Uh-oh."
+        }
+ 
 }
-// write an async function to fetch the stats, etc.
 
-
-
-fetch(urlWithKey)
-.then(function(response){
-    return response.json();
-})
-.then(function(json){
-    displayTeams(json)
-})
